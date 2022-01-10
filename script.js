7553fd5307b7cd411467aeedd34e6b51d4ae8818
Mueen <mueentaj1998@outlook.com>
2022-01-10 09:56:48 +0000

    Edit index.html and script.js

--- script.js
+++ script.js
@@ -1,41 +1,65 @@
-
-// playerSelection must select one of the three options (convert to all lower case)
-function playerSelection(){
-    user = window.prompt("Enter your choice here: ");
-    user_lower = user.toLowerCase();
-    return user_lower;
-}
-const person = playerSelection();
-
-// computerPlay must return either rock, paper or scissors randomly
 function computerPlay(){
     let choice = ["rock", "paper", "scissor"];
     let choices = choice[Math.floor(Math.random()*choice.length)];
     return choices;
 }
-const computer = computerPlay();
+let userScore = 0;
+let computerScore = 0;
+let draw = 0;
 
-//if statement and OR operator to compare the two values and to determine a winner
-// use a counter to keep track of score and to announce a winner
-// counter will be implemented once loop has been included
-let userScore = 0
-let computerScore = 0
-let draw = 0
-    
 function playRound(playerSelection, computerPlay){
     if (playerSelection == computerPlay){
-        console.log("This game is a draw!"); draw++;
+        console.log("This game is a draw!"); draw = draw++;
     } else if ((playerSelection == "rock" && computerPlay == "scissors") || 
     (playerSelection == "scissors" && computerPlay == "paper") || 
     (playerSelection == "paper" && computerPlay == "rock"))
-{ console.log("The user won!"); userScore++;
+{ console.log("The user won!"); userScore = userScore++;
 } else if ((computerPlay == "rock" && playerSelection == "scissors") || 
 (computerPlay == "scissors" && playerSelection == "paper") ||
 (computerPlay == "paper" && playerSelection == "rock")
-){ console.log("The computer won!"); computerScore++;
+){ console.log("The computer won!");computerScore = computerScore++;
  }
 }
-// use loop to make game play 5 times (not covered yet so will revisit later)
 
-console.log(playRound(person, computer));
+document.getElementById("myBtn1").addEventListener("click",myFunction => {
+    playRound("paper", computerPlay())
+    if(computerPlay == "paper"){
+        draw++;
+    }else if (computerPlay == "rock"){
+        userScore++;
+    } else if(computerPlay == "scissors"){
+        computerScore++;
+    }
+})
 
+document.getElementById("myBtn2").addEventListener("click", myFunction => {
+    playRound("rock", computerPlay())
+})
+
+document.getElementById("myBtn3").addEventListener("click", myFunction => {
+    playRound("scissors", computerPlay())
+})
+// need to add DOM to change console.log to on div displaying results, come back to fix this issue
+
+
+const divider = document.querySelector('#container');
+const div1 = document.createElement('div');
+divider.classList.add('divider')
+divider.appendChild(div1)
+document.getElementById("container").style.border = "thick solid #000000";
+document.getElementById("container").style.backgroundColor = "blue";
+
+const header1 = document.createElement('h1');
+header1.classList.add('header1');
+header1.textContent = "The user has this many wins: " + userScore;
+div1.appendChild(header1)
+
+const header2 = document.createElement('h1');
+header2.classList.add('header2');
+header2.textContent = "The computer has this many wins: " + computerScore;
+div1.appendChild(header2)
+
+const header3 = document.createElement('h1');
+header3.classList.add('header3');
+header3.textContent = "The user has this many wins: " + draw;
+div1.appendChild(header3)
